ed4334dda02c4d584884fed52b4665c914685b65
Mohammad Eko Nur Fauzi <61811112+mohammadoeko@users.noreply.github.com>
2022-11-04 20:20:12 +0700

    Update Order.java
    Update test order

--- src/main/java/com/mohammadeko/gitcollabs/pages/Order.java
+++ src/main/java/com/mohammadeko/gitcollabs/pages/Order.java
@@ -2,6 +2,6 @@ package com.mohammadeko.gitcollabs.pages;
 
 public class Order {
   public static void main(String[] args) {
-        System.out.println("Test");
+        System.out.println("Tes");
     }
 }
